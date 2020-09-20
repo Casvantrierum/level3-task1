f1e9bb6bc719010f2e8716cc5b8d09e83a2bf4b4
Cas <Casvantrierum@gmail.com>
2020-09-20 20:28:16 +0200

    Update build.gradle, MainActivity.kt, and 9 more files...

--- app/src/main/java/com/example/level3_task1/RatingFragment.kt
+++ app/src/main/java/com/example/level3_task1/RatingFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,8 +14,8 @@ import androidx.navigation.fragment.findNavController
 class RatingFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rating, container, false)
@@ -23,6 +23,28 @@ class RatingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        btn_to_summary.setOnClickListener {
+            navigateToSummary()
+        }
+
+        showRandomAssessableGame()
+    }
+
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf("Fall Guys", "Valorant",
+            "CS GO", "Minecraft").random()
+
+        txt_game.text = randomGame
+    }
+
+    private fun navigateToSummary() {
+
+        val args = Bundle()
+        args.putFloat(ARG_GAME_RATING, rb_game.rating)
+        args.putString(ARG_GAME_NAME, txt_game.text.toString())
+
+        findNavController().navigate(R.id.action_RatingFragment_to_SummaryFragment, args)
 
     }
 }
